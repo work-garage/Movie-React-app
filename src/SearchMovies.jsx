a506ca60f78c9e7ee03d7f7c7274b462632128c3
deepcodenetwork <skworkspace5@gmail.com>
2025-02-06 20:56:19 +0530

    update

--- src/SearchMovies.jsx
+++ src/SearchMovies.jsx
@@ -8,17 +8,23 @@ function SearchMovies({ addHistory, historySaved }) {
   const searchMovie = async (e) => {
     e.preventDefault();
     const url = `https://cors-anywhere.herokuapp.com/http://www.omdbapi.com/?apikey=1d6e9251&t=${movieTitle}`;
-    //For links use backticks(``) not single quotes('')
 
     try {
-      const response = await fetch(url); //fetch("_")    fetch data from url
-      const data = await response.json(); //val.json()  convert data to json
-      addHistory(data);
+      const response = await fetch(url, {
+        method: "GET", // Ensures we're making a GET request
+        headers: {
+          Origin: window.location.origin, // This is required by the CORS proxy
+          "X-Requested-With": "XMLHttpRequest", // This header is sometimes required as well
+        },
+      });
+      const data = await response.json(); // Convert data to JSON
+      addHistory(data); // Add the fetched data to the history
       console.log(historySaved);
     } catch (err) {
-      console.log(err);
+      console.log(err); // Log any errors
     }
   };
+
   return (
     <div className="search">
       <form onSubmit={searchMovie}>
@@ -28,17 +34,17 @@ function SearchMovies({ addHistory, historySaved }) {
             marginTop: "-4px",
             marginRight: "3px",
             height: "40px",
-            width: "40 px",
+            width: "40px",
           }}
           src={movieImg}
-          alt={"Movie "}
+          alt="Movie"
         />
-        <label htmlFor="query"> Movie Title </label>
+        <label htmlFor="query">Movie Title</label>
         <input
           type="text"
-          value={movieTitle} //value={movieTitle} 1) helps in Controlled Component for validation, where the value of the input field is managed by the state. 2)is pre entered default value in input bar, will get to see when given movieTitle some initial value
+          value={movieTitle}
           onChange={(e) => setMovieTitle(e.target.value)}
-          placeholder="Enter Movie Title" //placeholder="Enter Movie Title" is uneditable light coloured text you will see, when no value is entered
+          placeholder="Enter Movie Title"
           role="search"
           name="query"
           id="query"
@@ -52,26 +58,3 @@ function SearchMovies({ addHistory, historySaved }) {
 }
 
 export default SearchMovies;
-
-// arr.map((val)=>{ any work done by using val }) //val is each element of arr given one by one inside arrow function, like loop
-// arr.map((val,index)=>{ any work done by using val and index })   Parameter 1)each value 2)its index
-
-//to see Explaination of what is e and how its working
-
-//await,async
-
-//from where non outermost arrow func get values of their parameters? eg:- const searchMovie = async (e) => {}
-
-//Explain:
-/* const searchMovie = async (e) => {
-    e.preventDefault();
-    const url =
-      "http://www.omdbapi.com/?i=tt3896198&apikey=1d6e9251=${movieTitle}";
-
-    try {
-      const response = await fetch(url);
-      const data = await response.json();
-    } catch (err) {
-      console.log(err);
-    }
-*/
