fb4295290b3b982e71e000aaa8f5afa4d63918b3
deepcodenetwork <skworkspace5@gmail.com>
2025-02-06 20:50:57 +0530

    updated2

--- src/SearchMovies.jsx
+++ src/SearchMovies.jsx
@@ -1,17 +1,18 @@
 import { useState } from "react";
-import './SearchMovies.css';
-import movieImg from './movie.png';
+import "./SearchMovies.css";
+import movieImg from "./movie.png";
 
 function SearchMovies({ addHistory, historySaved }) {
   const [movieTitle, setMovieTitle] = useState("");
 
   const searchMovie = async (e) => {
     e.preventDefault();
-    const url = `http://www.omdbapi.com/?apikey=1d6e9251&t=${movieTitle}`;        //For links use backticks(``) not single quotes('')
+    const url = `https://cors-anywhere.herokuapp.com/http://www.omdbapi.com/?apikey=1d6e9251&t=${movieTitle}`;
+    //For links use backticks(``) not single quotes('')
 
     try {
-      const response = await fetch(url);                                          //fetch("_")    fetch data from url
-      const data = await response.json();                                         //val.json()  convert data to json
+      const response = await fetch(url); //fetch("_")    fetch data from url
+      const data = await response.json(); //val.json()  convert data to json
       addHistory(data);
       console.log(historySaved);
     } catch (err) {
@@ -20,26 +21,37 @@ function SearchMovies({ addHistory, historySaved }) {
   };
   return (
     <div className="search">
-      <form  onSubmit={searchMovie}>
-      <img style={{ marginBottom: "-7px",marginTop: "-4px",marginRight: "3px", height:"40px", width:"40 px"}} src={movieImg} alt={"Movie "} />
+      <form onSubmit={searchMovie}>
+        <img
+          style={{
+            marginBottom: "-7px",
+            marginTop: "-4px",
+            marginRight: "3px",
+            height: "40px",
+            width: "40 px",
+          }}
+          src={movieImg}
+          alt={"Movie "}
+        />
         <label htmlFor="query"> Movie Title </label>
         <input
           type="text"
-          value={movieTitle}                                 //value={movieTitle} 1) helps in Controlled Component for validation, where the value of the input field is managed by the state. 2)is pre entered default value in input bar, will get to see when given movieTitle some initial value
+          value={movieTitle} //value={movieTitle} 1) helps in Controlled Component for validation, where the value of the input field is managed by the state. 2)is pre entered default value in input bar, will get to see when given movieTitle some initial value
           onChange={(e) => setMovieTitle(e.target.value)}
-          placeholder="Enter Movie Title"                    //placeholder="Enter Movie Title" is uneditable light coloured text you will see, when no value is entered
+          placeholder="Enter Movie Title" //placeholder="Enter Movie Title" is uneditable light coloured text you will see, when no value is entered
           role="search"
           name="query"
           id="query"
         />
-        <button type="submit" style={{margin: "7px" }}>Search</button>
+        <button type="submit" style={{ margin: "7px" }}>
+          Search
+        </button>
       </form>
     </div>
   );
 }
 
 export default SearchMovies;
-
 
 // arr.map((val)=>{ any work done by using val }) //val is each element of arr given one by one inside arrow function, like loop
 // arr.map((val,index)=>{ any work done by using val and index })   Parameter 1)each value 2)its index
